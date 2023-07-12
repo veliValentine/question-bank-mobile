a550f56c84b6e6b499150cf05f63913f782d0cfe
unknown <nikkevanukas1995@gmail.com>
2023-07-12 22:23:44 +0300

    feat: question

---
+++ src/components/Question.jsx
@@ -0,0 +1,101 @@
+import { ScrollView, StyleSheet, Text, TouchableHighlight, TouchableOpacity, View } from 'react-native';
+
+const Question = ({
+  data,
+  nextQuestion,
+  previousQuestion,
+  isFirstQuestion,
+  isLastQuestion,
+  saveChoice
+}) => {
+  const { title, answers } = data;
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.title}>{title}</Text>
+        {answers.map((answer) => Option(answer, saveChoice))}
+      </ScrollView >
+      {Buttons({ previousQuestion, nextQuestion, isFirstQuestion, isLastQuestion })}
+    </View>
+  )
+}
+
+const Option = ({ choice, isCorrect }, saveChoice) => {
+  return (
+    <TouchableOpacity
+      key={choice}
+      onPress={() => saveChoice(isCorrect)}
+      style={styles.containerAnswer}
+    >
+      <Text style={styles.answer}>{choice}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const Buttons = ({ previousQuestion, nextQuestion, isFirstQuestion, isLastQuestion }) => {
+  return (
+    <View style={styles.buttonContainer}>
+      <TouchableHighlight
+        style={styles.button}
+        onPress={previousQuestion}
+        disabled={isFirstQuestion}
+      >
+        <Text style={styles.buttonText}>Previous</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        style={styles.button}
+        onPress={nextQuestion}
+      >
+        <Text style={styles.buttonText}>{isLastQuestion ? 'Finish' : 'Next'}</Text>
+      </TouchableHighlight>
+    </View>
+  )
+}
+
+export default Question;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#A8A196',
+    borderRadius: 20,
+    padding: 10,
+    flex: 1
+  },
+  containerAnswer: {
+    padding: 10,
+    backgroundColor: '#7D7463',
+    borderRadius: 20,
+    margin: 10
+  },
+  title: {
+    padding: 10,
+    color: 'black',
+    fontWeight: 'bold'
+  },
+  text: {
+    padding: 10,
+    color: 'black',
+  },
+  answer: {
+    padding: 10,
+  },
+  selectedAnswer: {
+    padding: 10,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  button: {
+    backgroundColor: '#9681EB',
+    borderRadius: 20,
+    margin: 10,
+    padding: 10,
+  },
+  buttonText: {
+    padding: 5,
+    color: 'black',
+  },
+});
