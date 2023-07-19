f0eff63f661213773f9ab3da15a4e1b827b222d3
unknown <nikkevanukas1995@gmail.com>
2023-07-19 16:33:19 +0300

    feat: confirm exit

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import Welcome from './src/Welcome';
 import Main from './src/Main';
+import { Alert, BackHandler } from 'react-native';
 
 const App = () => {
   const [showWelcome, setShowWelcome] = useState(true);
@@ -9,6 +10,27 @@ const App = () => {
     const timer = setTimeout(() => setShowWelcome(false), 5000);
     return () => { clearTimeout(timer) }
   }, [])
+
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert('Hold on!', 'Are you sure you want to exit?', [
+        {
+          text: 'Cancel',
+          onPress: () => null,
+          style: 'cancel',
+        },
+        { text: 'YES', onPress: () => BackHandler.exitApp() },
+      ]);
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction,
+    );
+
+    return () => backHandler.remove();
+  }, []);
 
   if (showWelcome) {
     return (
