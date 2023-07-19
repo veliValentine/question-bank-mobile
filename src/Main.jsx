5f3f981fb82d72e760934e3ebd8226b87de3f4e6
unknown <nikkevanukas1995@gmail.com>
2023-07-19 15:47:18 +0300

    feat: main menu

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,16 +1,17 @@
-import { StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import Quiz from './components/Quiz';
 import topics from './topics.json'
 import { useState } from 'react';
 import Results from './components/Results';
 
 const PAGES = {
+  MAIN_MENU: 'main_menu',
   QUIZ: 'quiz',
   RESULTS: 'results'
 }
 
 const Main = () => {
-  const [page, setPage] = useState(PAGES.QUIZ)
+  const [page, setPage] = useState(PAGES.MAIN_MENU)
   const [answers, setAnswers] = useState({})
   const allTopics = Object.keys(topics)
   const [topic] = useState(allTopics[0]);
@@ -21,8 +22,29 @@ const Main = () => {
     setAnswers(newAnswers)
   }
 
+  const startQuiz = () => {
+    setPage(PAGES.QUIZ)
+  }
+
   const finishQuiz = () => {
     setPage(PAGES.RESULTS)
+  }
+
+  const onNewQuiz = () => {
+    setAnswers({})
+    setPage(PAGES.MAIN_MENU)
+  }
+
+  if (page === PAGES.RESULTS) {
+    return (
+      <View style={styles.container}>
+        <Results
+          results={answers}
+          questions={questions}
+          onNewQuiz={onNewQuiz}
+        />
+      </View>
+    )
   }
 
   if (page === PAGES.QUIZ) {
@@ -38,17 +60,19 @@ const Main = () => {
     )
   }
 
-  const onNewQuiz = () => {
-    setAnswers({})
-    setPage(PAGES.QUIZ)
-  }
-
   return (
     <View style={styles.container}>
-      <Results
-        results={answers}
-        questions={questions}
-        onNewQuiz={onNewQuiz}
+      <MainMenu startQuiz={startQuiz} />
+    </View>
+  )
+}
+
+const MainMenu = ({ startQuiz }) => {
+  return (
+    <View style={styles.mainMenuContainer}>
+      <Button
+        onPress={startQuiz}
+        title='Start DA-42 quiz'
       />
     </View>
   )
@@ -61,5 +85,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#F4E0B9',
     height: "100%",
     padding: 50
+  },
+  mainMenuContainer: {
+    padding: 10,
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center'
   }
 });
